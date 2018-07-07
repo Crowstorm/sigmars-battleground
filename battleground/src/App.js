41e5cc41a776243b3209fc9fd456ec26c2616c32
Crowstorm <crowstormofficial@gmail.com>
2018-07-07 14:26:07 +0200

    react native navigation setup

--- battleground/src/App.js
+++ battleground/src/App.js
@@ -1,25 +1,15 @@
-import React from 'react';
-import { Provider } from 'react-redux'
+import {Navigation} from 'react-native-navigation';
 
-import { StyleSheet, Text, View } from 'react-native';
+import SplashScreen from './screens/splash';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+//register screens
+Navigation.registerComponent("battleground.SplashScreen", ()=> SplashScreen);
+
+//start app
+
+Navigation.startSingleScreenApp({
+  screen: {
+    screen: "battleground.SplashScreen",
+    title: "Welcome"
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+})
---
+++ battleground/src/screens/splash.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { View, Text } from 'react-native';
+
+class SplashScreen extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text> Test </Text>
+            </View>
+        )
+    }
+}
+
+export default SplashScreen;
