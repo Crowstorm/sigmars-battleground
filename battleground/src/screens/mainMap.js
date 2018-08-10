484ecea65ec520f34759e5d620cd668502bb3c1a
Crowstorm <crowstormofficial@gmail.com>
2018-08-10 21:25:19 +0200

    grid prototype

--- battleground/src/screens/mainMap.js
+++ battleground/src/screens/mainMap.js
@@ -1,16 +1,84 @@
 import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-import { View, Text } from 'react-native';
+import SquareGrid from "react-native-square-grid";
 
 class MainMapScreen extends React.Component {
     render() {
-        console.log('mapa')
+
         return (
-            <View>
-                <Text> to bedzie mapa</Text>
+            <View style={styles.container}>
+                <View style={styles.mapContainer}>
+                    <View style={styles.mainSquare}>
+                        <View style={styles.smallSquare}></View>
+                        <View style={styles.smallSquare}></View>
+                        <View style={styles.smallSquare}></View>
+                        <View style={styles.smallSquare}></View>
+                        <View style={styles.smallSquare}></View>
+                        <View style={styles.smallSquare}></View>
+                        <View style={styles.smallSquare}></View>
+                        <View style={styles.smallSquare}></View>
+                        <View style={styles.smallSquare}></View>
+                    </View>
+                    <View style={styles.mainSquare}></View>
+                    <View style={styles.mainSquare}></View>
+                    <View style={styles.mainSquare}></View>
+                    <View style={styles.mainSquare}></View>
+                    <View style={styles.mainSquare}></View>
+
+                </View>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        borderColor: "red",
+        borderWidth: 1,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    mapContainer: {
+        borderColor: "blue",
+        borderWidth: 1,
+        width: "80%",
+        height: "70%",
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        aspectRatio: 2 / 3,
+    },
+    mainSquare: {
+        borderColor: "green",
+        borderWidth: 1,
+        width: '50%',
+        aspectRatio: 1,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+    },
+    smallSquare:{
+        borderColor: "pink",
+        borderWidth: 1,
+        width: '33%',
+        aspectRatio: 1,
+    }
+    // item: {
+    //     flex: 1,
+    //     alignSelf: "stretch",
+    //     // padding: 16
+    // },
+    // content: {
+    //     flex: 1,
+    //     borderColor: "green",
+    //     borderWidth: 1,
+    //     alignItems: "center",
+    //     justifyContent: "center"
+    // },
+    // text: {
+    //     color: "white",
+    //     fontSize: 32
+    // }
+})
 
 export default MainMapScreen;
