c4a086de6a8f37c6e24756a7483924327cc567ca
Crowstorm <crowstormofficial@gmail.com>
2018-08-27 21:58:29 +0200

    basic reroll of single dice

--- battleground/src/screens/dice.js
+++ battleground/src/screens/dice.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import _ from 'lodash';
 import { View, Text, TextInput, TouchableOpacity, Button, Image, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 import LinearGradient from 'react-native-linear-gradient';
 
 import { setNumberOfDice, rollTheDice, rerollDice } from '../store/actions/dice';
+
+import dice1 from '../assets/dice/inverted-dice-1.png';
+import dice2 from '../assets/dice/inverted-dice-2.png';
+import dice3 from '../assets/dice/inverted-dice-3.png';
+import dice4 from '../assets/dice/inverted-dice-4.png';
+import dice5 from '../assets/dice/inverted-dice-5.png';
+import dice6 from '../assets/dice/inverted-dice-6.png';
 
 
 class DiceScreen extends React.Component {
@@ -24,13 +32,38 @@ class DiceScreen extends React.Component {
         this.props.rollTheDice();
     }
 
-    handleReroll = (value) =>{
+    handleReroll = (value) => {
         this.props.rerollDice(value);
+    }
+
+    renderDiceReroll = () => {
+        const dice = [
+            { 'image': dice1, 'number': 1 },
+            { 'image': dice2, 'number': 2 },
+            { 'image': dice3, 'number': 3 },
+            { 'image': dice4, 'number': 4 },
+            { 'image': dice5, 'number': 5 },
+            { 'image': dice6, 'number': 6 }
+        ];
+
+        return _.map(dice, (d) => {
+            return (
+                <View key={d.number}>
+                    <TouchableOpacity style={styles.rollButton} onPress={() => this.handleReroll(d.number)} >
+                            <View style={styles.rerollDice}>
+                                <Image source={d.image} style={styles.rerollDice} />
+                            </View>
+                    </TouchableOpacity>
+                </View>
+            )
+        });
     }
 
     render() {
         console.log(this.props)
         let { one, two, three, four, five, six } = this.props.dice;
+
+        let renderDiceReroll = this.renderDiceReroll();
         return (
             <LinearGradient colors={['#4c669f', '#3b5998', '#192f6a']} style={styles.container}>
                 <Text> Number of dice</Text>
@@ -46,14 +79,10 @@ class DiceScreen extends React.Component {
                     <Text>Number of 3s and above:{six + five + four + three}</Text>
                     <Text>Number of 2s and above:{six + five + four + three + two}</Text>
                     <Text>Number of 1s:{one}</Text>
+                </View>
 
-                    <TouchableOpacity style={styles.rollButton} onPress={() => this.handleReroll(2)} >
-                        <LinearGradient colors={['#192f6a', '#3b5998', '#192f6a']} style={{ borderRadius: 10 }}>
-                            <View>
-                                <Text style={styles.rollButtonText}>Reroll</Text>
-                            </View>
-                        </LinearGradient>
-                    </TouchableOpacity>
+                <View style={styles.rerollView}>
+                    {renderDiceReroll}
                 </View>
                 <TouchableOpacity style={styles.rollButton} onPress={this.handleRollDice} >
                     <LinearGradient colors={['#192f6a', '#3b5998', '#192f6a']} style={{ borderRadius: 10 }}>
@@ -96,10 +125,6 @@ const styles = StyleSheet.create({
         borderWidth: 2,
         borderRadius: 10,
         marginTop: "10%",
-        //alignSelf: 'flex-end'
-        // position: 'absolute',
-        // bottom: "50%",
-        // left: "50%",
     },
     rollButtonText: {
         color: "#AB7A30",
@@ -109,6 +134,13 @@ const styles = StyleSheet.create({
         fontWeight: "bold",
         fontSize: 16,
         padding: 10
+    },
+    rerollView:{
+        flexDirection: "row"
+    },
+    rerollDice:{
+        height: 50,
+        width: 50
     }
 
 })
@@ -117,7 +149,7 @@ const mapDispatchToProps = dispatch => {
     return {
         setNumberOfDice: (value) => dispatch(setNumberOfDice(value)),
         rollTheDice: () => dispatch(rollTheDice()),
-        rerollDice: (value) =>dispatch(rerollDice(value)),
+        rerollDice: (value) => dispatch(rerollDice(value)),
     }
 }
 
