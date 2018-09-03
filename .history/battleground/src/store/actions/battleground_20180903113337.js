af9eef881c318180377b51abf4dadf483d2b31d0
Mateusz Mazur <mateuszmazurofficial@gmail.com>
2018-09-03 11:38:07 +0200

    toggle visibility on generate map

---
+++ .history/battleground/src/store/actions/battleground_20180903113337.js
@@ -0,0 +1,139 @@
+import store from '../configureStore';
+
+export const toggleTerrain = () =>{
+    return {
+        type: 'TOGGLE_TERRAIN',
+    }
+}
+
+export const toggleScenery = () =>{
+    return {
+        type: 'TOGGLE_SCENERY',
+    }
+}
+
+export const incrementHelperIndex = () =>{
+    return{
+        type: 'INCREMENT_INDEX_HELPER',
+    }
+}
+
+export const zeroHelperIndex = () =>{
+    return{
+        type: 'ZERO_INDEX_HELPER',
+    }
+}
+
+export const setTerrainFlag = (index1, index2) =>{
+    return{
+        type: 'SET_TERRAIN_FLAG',
+        index1,
+        index2
+    }
+}
+
+export const pickSquares = () =>{
+    return function (dispatch, getState) {
+        let value = [];
+        for(let i = 0; i <6; i++){
+            let shuffled = shuffle(getState().battleground.squareIndex);
+            let selected = shuffled.slice(0, getState().battleground.numberOfTerrains[i]);
+            let terrainInfo = [];
+            for(let j =0; j < getState().battleground.numberOfTerrains[i]; j++){
+                //0:square index, 1: terrain 2: scenery
+                let temp = [];
+                //get square
+                temp.push(selected[j])
+                //roll for terrain
+                temp.push(pickTerrainIndex());
+                //roll for scenery
+                temp.push(singleDiceRoll());
+                //push flags for inserting terrain and scenery
+                temp.push(false, false)
+                //push to terrainInfo
+                terrainInfo.push(temp)
+            }
+            value.push(terrainInfo);
+        }
+        this.toggleScenery();
+        this.toggleTerrain();
+        dispatch({
+            type: 'PICKED_SQUARES',
+            value
+        })
+    }
+
+}
+
+export const rollNumberOfTerrains = () =>{
+     let value = [];
+     for(let i = 0; i < 6; i++){
+        let diceRoll = doubleDiceRoll();
+        value.push(getNumberOfTerrains(diceRoll));
+     }
+    return{
+        type: 'TERRAIN_AMOUNT',
+        value
+    }
+}
+
+export const toggleMapLegend = () =>{
+    return{
+        type: 'TOGGLE_MAP_LEGEND'
+    }
+}
+
+const singleDiceRoll = () => {
+    return Math.floor(Math.random() * 6 + 1);
+}
+
+const pickTerrainIndex = () => {
+    return Math.floor(Math.random() * 5);
+}
+
+const doubleDiceRoll = () => {
+    let firstDice = singleDiceRoll();
+    let secondDice = singleDiceRoll();
+    let total = firstDice + secondDice;
+    return total;
+}
+
+const getNumberOfTerrains = (diceRoll) => {
+    switch (diceRoll) {
+        case 2:
+        case 3:
+            return 0;
+        case 4:
+        case 5:
+        case 9:
+        case 10:
+            return 2;
+        case 6:
+        case 7:
+        case 8:
+            return 1;
+        case 11:
+        case 12:
+            return 3;
+    }
+}
+
+//shuffling array algorythm
+const shuffle = (array) => {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
