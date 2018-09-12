142209530ee0020f4c912b21051413ccf0c80623
Mateusz Mazur <mateuszmazurofficial@gmail.com>
2018-09-12 10:46:03 +0200

    pdf picker add

---
+++ .history/battleground/src/screens/components/addImage_20180912103309.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {View, Text, Button, Image, AsyncStorage} from 'react-native';
+import ImagePicker from 'react-native-image-picker';
+import { DocumentPicker, DocumentPickerUtil } from 'react-native-document-picker';
+
+class AddImage extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            file: 'nic'
+        }
+    }
+    handleFilePick = () =>{
+        DocumentPicker.show({
+            filetype: [DocumentPickerUtil.images()],
+          },(error,res) => {
+            // Android
+            this.setState({
+                file: res
+            })
+            console.log(
+               res.uri,
+               res.type, // mime type
+               res.fileName,
+               res.fileSize
+            );
+          });
+      
+    }
+    render(){
+        return(
+            <View>
+                <Text>Add image</Text>
+                <Button title="Pick a file" onPress={() => this.handleFilePick()} />
+            </View>
+        )
+    }
+}
+
+export default AddImage;
